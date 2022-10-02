7813be6894251e3239ca1c2438cf94ad91070791
Андрей Легкий <legkiy.andrey@bk.ru>
2022-10-02 11:23:48 +0300

    1

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -63,15 +63,17 @@ class PostsPagesTests(TestCase):
         for url, template, agr in self.paginated_urls:
             templates_pages_names = {
                 reverse(self.index_url[0]): self.index_url[1],
-                reverse(self.group_url[0], kwargs={'slug': self.group_url[2]}):
+                reverse(self.group_url[0],
+                        kwargs={'slug': self.group_url[2]}):
                 self.group_url[1],
                 reverse(self.profile_url[0],
-                    kwargs={'username': self.profile_url[2]}):
+                        kwargs={'username': self.profile_url[2]}):
                 self.profile_url[1],
-                reverse(self.post_url[0], kwargs={'post_id': self.post_url[2]}):
+                reverse(self.post_url[0],
+                        kwargs={'post_id': self.post_url[2]}):
                 self.post_url[1],
                 reverse(self.edit_post_url[0],
-                    kwargs={'post_id': self.edit_post_url[2]}):
+                        kwargs={'post_id': self.edit_post_url[2]}):
                 self.edit_post_url[1],
                 reverse(self.new_post_url[0]): self.new_post_url[1],
             }
